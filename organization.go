5ec9b2df5e39ee9f65e28e9ae6cf6715051bc261
Max Trense <dev@trense.info>
2021-05-23 12:51:22 +0200

    Ad queries for Repositories and connected information

--- organization.go
+++ organization.go
@@ -1,5 +1,347 @@
 package github
 
+import (
+	"time"
+
+	"github.com/shurcooL/githubv4"
+)
+
 type Organization struct {
 	Name string
+}
+
+type Member struct {
+	Login               string    `json:"login,omitempty"`
+	Name                string    `json:"name,omitempty"`
+	Email               string    `json:"email,omitempty"`
+	HasTwoFactorEnabled bool      `json:"has_two_factor_enabled,omitempty"`
+	Role                string    `json:"role,omitempty"`
+	CreatedAt           time.Time `json:"created_at,omitempty"`
+	Pending             bool      `json:"pending"`
+}
+
+type MemberOption func()
+
+func (s *GithubClient) GetMembers(org string, opts ...MemberOption) ([]*Member, error) {
+	var members []*Member
+	if nonPendingMembers, err := s.getNonPendingMembers(org); err == nil {
+		for _, m := range nonPendingMembers {
+			members = append(members, m)
+		}
+	} else {
+		return nil, err
+	}
+	if pendingMembers, err := s.getPendingMembers(org); err == nil {
+		for _, m := range pendingMembers {
+			members = append(members, m)
+		}
+	} else {
+		return nil, err
+	}
+	return members, nil
+}
+
+func (s *GithubClient) getNonPendingMembers(org string) ([]*Member, error) {
+	var query struct {
+		Organization struct {
+			Login           githubv4.String
+			MembersWithRole struct {
+				PageInfo PageInfo
+				Edges    []struct {
+					Node struct {
+						Login     githubv4.String
+						Name      githubv4.String
+						Email     githubv4.String
+						CreatedAt githubv4.DateTime
+					}
+					HasTwoFactorEnabled githubv4.Boolean
+					Role                githubv4.String
+				}
+			} `graphql:"membersWithRole(first: 100, after: $cursor)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	var members []*Member
+	return members, s.Query(&query).Str("org", org).Cursor("cursor").Run(func() PageInfo {
+		for _, edge := range query.Organization.MembersWithRole.Edges {
+			m := &Member{
+				Login:               string(edge.Node.Login),
+				Name:                string(edge.Node.Name),
+				Email:               string(edge.Node.Email),
+				HasTwoFactorEnabled: bool(edge.HasTwoFactorEnabled),
+				Role:                string(edge.Role),
+				CreatedAt:           edge.Node.CreatedAt.Time,
+				Pending:             false,
+			}
+			members = append(members, m)
+		}
+		return query.Organization.MembersWithRole.PageInfo
+	})
+}
+
+func (s *GithubClient) getPendingMembers(org string) ([]*Member, error) {
+	var query struct {
+		Organization struct {
+			Login          githubv4.String
+			PendingMembers struct {
+				PageInfo PageInfo
+				Nodes    []struct {
+					Login     githubv4.String
+					Name      githubv4.String
+					Email     githubv4.String
+					CreatedAt githubv4.DateTime
+				} `graphql:"nodes"`
+			} `graphql:"pendingMembers(first: 100, after: $cursor)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	var members []*Member
+	return members, s.Query(&query).Str("org", org).Cursor("cursor").Run(func() PageInfo {
+		for _, node := range query.Organization.PendingMembers.Nodes {
+			m := &Member{
+				Login:     string(node.Login),
+				Name:      string(node.Name),
+				Email:     string(node.Email),
+				CreatedAt: node.CreatedAt.Time,
+				Pending:   true,
+			}
+			members = append(members, m)
+		}
+		return query.Organization.PendingMembers.PageInfo
+	})
+}
+
+type Team struct {
+	ID              string            `json:"id,omitempty"`
+	Name            string            `json:"name,omitempty"`
+	Slug            string            `json:"slug,omitempty"`
+	CombinedSlug    string            `json:"combined_slug,omitempty"`
+	Parent          string            `json:"parent,omitempty"`
+	MemberCount     int               `json:"member_count,omitempty"`
+	Members         []*TeamMember     `json:"members,omitempty"`
+	RepositoryCount int               `json:"repository_count,omitempty"`
+	Repositories    []*TeamRepository `json:"repositories,omitempty"`
+	ChildCount      int               `json:"child_count,omitempty"`
+}
+
+func (s *GithubClient) GetTeams(org string) ([]*Team, error) {
+	var query struct {
+		Organization struct {
+			Teams struct {
+				PageInfo PageInfo
+				Nodes    []struct {
+					ID           githubv4.String
+					Name         githubv4.String
+					Slug         githubv4.String
+					CombinedSlug githubv4.String
+					ParentTeam   struct {
+						ID githubv4.String
+					}
+					Members struct {
+						TotalCount githubv4.Int
+					}
+					Repositories struct {
+						TotalCount githubv4.Int
+					}
+					ChildTeams struct {
+						TotalCount githubv4.Int
+					}
+				}
+			} `graphql:"teams(first: 100, after: $cursor)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	var teams []*Team
+	return teams, s.Query(&query).Str("org", org).Cursor("cursor").Run(func() PageInfo {
+		for _, node := range query.Organization.Teams.Nodes {
+			t := &Team{
+				ID:              string(node.ID),
+				Name:            string(node.Name),
+				Slug:            string(node.Slug),
+				CombinedSlug:    string(node.CombinedSlug),
+				Parent:          string(node.ParentTeam.ID),
+				MemberCount:     int(node.Members.TotalCount),
+				RepositoryCount: int(node.Repositories.TotalCount),
+				ChildCount:      int(node.ChildTeams.TotalCount),
+			}
+			teams = append(teams, t)
+		}
+		return query.Organization.Teams.PageInfo
+	})
+}
+
+type TeamMember struct {
+	Login string `json:"login,omitempty"`
+	Role  string `json:"role,omitempty"`
+}
+
+func (s *GithubClient) LoadTeamMembers(org string, teams ...*Team) error {
+	for _, team := range teams {
+		if err := s.loadTeamMembers(org, team); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadTeamMembers(org string, team *Team) error {
+	var query struct {
+		Organization struct {
+			Team struct {
+				Members struct {
+					PageInfo PageInfo
+					Edges    []struct {
+						Node struct {
+							Login githubv4.String
+						}
+						Role githubv4.String
+					}
+				} `graphql:"members(first: 100, after: $cursor)"`
+			} `graphql:"team(slug: $slug)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	return s.Query(&query).Str("org", org).Str("slug", team.Slug).Cursor("cursor").Run(func() PageInfo {
+		for _, m := range query.Organization.Team.Members.Edges {
+			teamMember := &TeamMember{
+				Login: string(m.Node.Login),
+				Role:  string(m.Role),
+			}
+			team.Members = append(team.Members, teamMember)
+		}
+		return query.Organization.Team.Members.PageInfo
+	})
+}
+
+type TeamRepository struct {
+	Owner      string `json:"owner,omitempty"`
+	Name       string `json:"name,omitempty"`
+	Permission string `json:"permission,omitempty"`
+}
+
+func (s *GithubClient) LoadTeamRepositories(org string, teams ...*Team) error {
+	for _, team := range teams {
+		if err := s.loadTeamRepositories(org, team); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadTeamRepositories(org string, team *Team) error {
+	var query struct {
+		Organization struct {
+			Team struct {
+				Repositories struct {
+					PageInfo PageInfo
+					Edges    []struct {
+						Node struct {
+							Owner struct {
+								Login githubv4.String
+							}
+							Name githubv4.String
+						}
+						Permission githubv4.String
+					}
+				} `graphql:"repositories(first: 100, after: $cursor)"`
+			} `graphql:"team(slug: $slug)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	return s.Query(&query).Str("org", org).Str("slug", team.Slug).Cursor("cursor").Run(func() PageInfo {
+		for _, m := range query.Organization.Team.Repositories.Edges {
+			teamRepository := &TeamRepository{
+				Owner:      string(m.Node.Owner.Login),
+				Name:       string(m.Node.Name),
+				Permission: string(m.Permission),
+			}
+			team.Repositories = append(team.Repositories, teamRepository)
+		}
+		return query.Organization.Team.Repositories.PageInfo
+	})
+}
+
+func (s *GithubClient) GetOrganizationRepositories(org string) ([]*Repository, error) {
+	var query struct {
+		Organization struct {
+			Repositories struct {
+				PageInfo PageInfo
+				Nodes    []struct {
+					Owner struct {
+						Login githubv4.String
+					}
+					Name             githubv4.String
+					CreatedAt        githubv4.DateTime
+					DefaultBranchRef struct {
+						Name githubv4.String
+					}
+					Description          githubv4.String
+					DescriptionHTML      githubv4.String `graphql:"descriptionHTML"`
+					ShortDescriptionHTML githubv4.String `graphql:"shortDescriptionHTML"`
+					HasIssuesEnabled     githubv4.Boolean
+					HasProjectsEnabled   githubv4.Boolean
+					HasWikiEnabled       githubv4.Boolean
+					HomepageURL          githubv4.String
+					URL                  githubv4.String
+					SshURL               githubv4.String
+					IsArchived           githubv4.Boolean
+					IsDisabled           githubv4.Boolean
+					IsEmpty              githubv4.Boolean
+					IsFork               githubv4.Boolean
+					IsLocked             githubv4.Boolean
+					IsMirror             githubv4.Boolean
+					IsPrivate            githubv4.Boolean
+					IsTemplate           githubv4.Boolean
+					LicenseInfo          struct {
+						Name githubv4.String
+					}
+					Labels struct {
+						TotalCount githubv4.Int
+					}
+					Languages struct {
+						TotalCount githubv4.Int
+					}
+					RebaseMergeAllowed  githubv4.Boolean
+					MergeCommitAllowed  githubv4.Boolean
+					SquashMergeAllowed  githubv4.Boolean
+					DeleteBranchOnMerge githubv4.Boolean
+					DiskUsage           githubv4.Int
+					PushedAt            githubv4.DateTime
+					UpdatedAt           githubv4.DateTime
+					PrimaryLanguage     struct {
+						Name githubv4.String
+					}
+				}
+			} `graphql:"repositories(first: 100, after: $cursor)"`
+		} `graphql:"organization(login: $org)"`
+	}
+	var repositories []*Repository
+	return repositories, s.Query(&query).Str("org", org).Cursor("cursor").Run(func() PageInfo {
+		for _, node := range query.Organization.Repositories.Nodes {
+			r := &Repository{
+				Owner:               string(node.Owner.Login),
+				Name:                string(node.Name),
+				Private:             bool(node.IsPrivate),
+				Description:         string(node.Description),
+				Homepage:            string(node.HomepageURL),
+				URL:                 string(node.URL),
+				Ssh:                 string(node.SshURL),
+				DefaultBranch:       string(node.DefaultBranchRef.Name),
+				Archived:            bool(node.IsArchived),
+				Disabled:            bool(node.IsDisabled),
+				Template:            bool(node.IsTemplate),
+				IssuesEnabled:       bool(node.HasIssuesEnabled),
+				ProjectsEnabled:     bool(node.HasProjectsEnabled),
+				WikiEnabled:         bool(node.HasWikiEnabled),
+				AllowRebaseMerge:    bool(node.RebaseMergeAllowed),
+				AllowSquashMerge:    bool(node.SquashMergeAllowed),
+				AllowMergeCommit:    bool(node.MergeCommitAllowed),
+				DeleteBranchOnMerge: bool(node.DeleteBranchOnMerge),
+				LicenseName:         string(node.LicenseInfo.Name),
+				Topics:              nil,
+				DiskUsage:           int(node.DiskUsage),
+				PushedAt:            node.PushedAt.Time,
+				UpdatedAt:           node.UpdatedAt.Time,
+				Age:                 int(time.Now().Sub(node.PushedAt.Time).Seconds()),
+				PrimaryLanguage:     string(node.PrimaryLanguage.Name),
+			}
+			repositories = append(repositories, r)
+		}
+		return query.Organization.Repositories.PageInfo
+	})
 }
--- repositories.go
+++ repositories.go
@@ -2,70 +2,331 @@ package github
 
 import (
 	"context"
+	"time"
+
+	log "github.com/mtrense/soil/logging"
+
+	"github.com/shurcooL/githubv4"
 
 	gh3 "github.com/google/go-github/v32/github"
 )
 
 type Repository struct {
-	Name             string   `json:"name,omitempty"`
-	Private          bool     `json:"private,omitempty"`
-	IsArchived       bool     `json:"is_archived,omitempty"`
-	IsDisabled       bool     `json:"is_disabled,omitempty"`
-	Topics           []string `json:"topics,omitempty"`
-	Description      string   `json:"description,omitempty"`
-	Homepage         string   `json:"homepage,omitempty"`
-	Owner            string   `json:"owner,omitempty"`
-	URL              string   `json:"url,omitempty"`
-	Ssh              string   `json:"ssh,omitempty"`
-	Git              string   `json:"git,omitempty"`
-	HasIssues        bool     `json:"has_issues,omitempty"`
-	HasWiki          bool     `json:"has_wiki,omitempty"`
-	HasProject       bool     `json:"has_project,omitempty"`
-	HasPages         bool     `json:"has_pages,omitempty"`
-	IsTemplate       bool     `json:"is_template,omitempty"`
-	AllowRebaseMerge bool     `json:"allow_rebase_merge,omitempty"`
-	AllowSquashMerge bool     `json:"allow_squash_merge,omitempty"`
-	AllowMergeCommit bool     `json:"allow_merge_commit,omitempty"`
+	Name                  string                 `json:"name,omitempty"`
+	Private               bool                   `json:"private,omitempty"`
+	Description           string                 `json:"description,omitempty"`
+	Homepage              string                 `json:"homepage,omitempty"`
+	Owner                 string                 `json:"owner,omitempty"`
+	URL                   string                 `json:"url,omitempty"`
+	Ssh                   string                 `json:"ssh,omitempty"`
+	Git                   string                 `json:"git,omitempty"`
+	DefaultBranch         string                 `json:"default_branch,omitempty"`
+	Archived              bool                   `json:"archived,omitempty"`
+	Disabled              bool                   `json:"disabled,omitempty"`
+	Template              bool                   `json:"template,omitempty"`
+	IssuesEnabled         bool                   `json:"issues_enabled,omitempty"`
+	WikiEnabled           bool                   `json:"wiki_enabled,omitempty"`
+	ProjectsEnabled       bool                   `json:"projects_enabled,omitempty"`
+	PagesEnabled          bool                   `json:"pages_enabled,omitempty"`
+	AllowRebaseMerge      bool                   `json:"allow_rebase_merge,omitempty"`
+	AllowSquashMerge      bool                   `json:"allow_squash_merge,omitempty"`
+	AllowMergeCommit      bool                   `json:"allow_merge_commit,omitempty"`
+	DeleteBranchOnMerge   bool                   `json:"delete_branch_on_merge,omitempty"`
+	VulnerabilityAlerts   bool                   `json:"vulnerability_alerts,omitempty"`
+	LicenseName           string                 `json:"license_name,omitempty"`
+	Topics                []string               `json:"topics,omitempty"`
+	Collaborators         []Collaborator         `json:"collaborators,omitempty"`
+	BranchProtectionRules []BranchProtectionRule `json:"branch_protection_rules,omitempty"`
+	DiskUsage             int                    `json:"disk_usage,omitempty"`
+	PushedAt              time.Time              `json:"pushed_at,omitempty"`
+	UpdatedAt             time.Time              `json:"updated_at,omitempty"`
+	Age                   int                    `json:"age,omitempty"`
+	PrimaryLanguage       string                 `json:"primary_language,omitempty"`
+	Languages             []Language             `json:"languages,omitempty"`
+	Workflows             []Workflow             `json:"workflows,omitempty"`
 }
 
-func fromRepo(r *gh3.Repository) Repository {
-	return Repository{
-		Name:             unboxString(r.Name),
-		Private:          *r.Private,
-		IsArchived:       unboxBool(r.Archived),
-		IsDisabled:       unboxBool(r.Disabled),
-		Topics:           r.Topics,
-		Description:      unboxString(r.Description),
-		Homepage:         unboxString(r.Homepage),
-		Owner:            unboxString(r.Owner.Login),
-		URL:              unboxString(r.URL),
-		Ssh:              unboxString(r.SSHURL),
-		Git:              unboxString(r.GitURL),
-		HasIssues:        unboxBool(r.HasIssues),
-		HasWiki:          unboxBool(r.HasWiki),
-		HasProject:       unboxBool(r.HasProjects),
-		HasPages:         unboxBool(r.HasPages),
-		AllowRebaseMerge: unboxBool(r.AllowRebaseMerge),
-		AllowSquashMerge: unboxBool(r.AllowSquashMerge),
-		AllowMergeCommit: unboxBool(r.AllowMergeCommit),
+type Language struct {
+	Name        string `json:"name,omitempty"`
+	LinesOfCode int    `json:"lines_of_code,omitempty"`
+}
+
+func (s *GithubClient) LoadRepositoryLanguages(repositories ...*Repository) error {
+	for _, repository := range repositories {
+		if err := s.loadRepositoryLanguages(repository); err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func (s *GithubClient) ListOrgRepositories(org string) ([]Repository, error) {
-	var result []Repository
-	err := s.paginateGithub3(func(lo gh3.ListOptions) (*gh3.Response, error) {
-		repos, resp, err := s.v3Client.Repositories.ListByOrg(context.Background(), org, &gh3.RepositoryListByOrgOptions{
-			ListOptions: lo,
-		})
-		for _, r := range repos {
-			result = append(result, fromRepo(r))
+func (s *GithubClient) loadRepositoryLanguages(repository *Repository) error {
+	var query struct {
+		Repository struct {
+			Languages struct {
+				PageInfo PageInfo
+				Edges    []struct {
+					Node struct {
+						Name githubv4.String
+					}
+					Size githubv4.Int
+				}
+			} `graphql:"languages(first: 100, after: $cursor)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+	return s.Query(&query).Str("owner", repository.Owner).Str("repo", repository.Name).Cursor("cursor").Run(func() PageInfo {
+		for _, lang := range query.Repository.Languages.Edges {
+			language := Language{
+				Name:        string(lang.Node.Name),
+				LinesOfCode: int(lang.Size),
+			}
+			repository.Languages = append(repository.Languages, language)
+		}
+		return query.Repository.Languages.PageInfo
+	})
+}
+
+type Collaborator struct {
+	Login               string             `json:"login,omitempty"`
+	EffectivePermission string             `json:"effective_permission,omitempty"`
+	Sources             []PermissionSource `json:"sources,omitempty"`
+}
+
+type PermissionSource struct {
+	Organization    string `json:"organization,omitempty"`
+	Permission      string `json:"permission,omitempty"`
+	SourceType      string `json:"source_type,omitempty"`
+	TeamName        string `json:"team_name,omitempty"`
+	TeamID          string `json:"team_id,omitempty"`
+	RepositoryOwner string `json:"repository_owner,omitempty"`
+	RepositoryName  string `json:"repository_name,omitempty"`
+}
+
+func (s *GithubClient) LoadRepositoryCollaborators(repositories ...*Repository) error {
+	for _, repository := range repositories {
+		if !repository.Archived {
+			if err := s.loadRepositoryCollaborators(repository); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadRepositoryCollaborators(repository *Repository) error {
+	var query struct {
+		Repository struct {
+			Collaborators struct {
+				PageInfo PageInfo
+				Edges    []struct {
+					Node struct {
+						Login githubv4.String
+					}
+					Permission        githubv4.String
+					PermissionSources []struct {
+						Organization struct {
+							Name githubv4.String
+						}
+						Permission githubv4.String
+						Source     struct {
+							Typename githubv4.String `graphql:"__typename"`
+							Team     struct {
+								Name githubv4.String
+								ID   githubv4.String
+							} `graphql:"... on Team"`
+							Repository struct {
+								Owner struct {
+									Login githubv4.String
+								}
+								Name githubv4.String
+							} `graphql:"... on Repository"`
+							Organization struct {
+								Name githubv4.String
+							} `graphql:"... on Organization"`
+						}
+					}
+				}
+			} `graphql:"collaborators(first: 10, after: $cursor)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+	log.L().Info().Str("repo", repository.Name).Msg("Fetching Collaborators")
+	return s.Query(&query).Str("owner", repository.Owner).Str("repo", repository.Name).Cursor("cursor").Run(func() PageInfo {
+		log.L().Info().Bool("next-page", bool(query.Repository.Collaborators.PageInfo.HasNextPage)).Str("repo", repository.Name).Msg("Fetched next page")
+		for _, coll := range query.Repository.Collaborators.Edges {
+			var sources []PermissionSource
+			for _, ps := range coll.PermissionSources {
+				permissionSource := PermissionSource{
+					Organization:    string(ps.Organization.Name),
+					Permission:      string(ps.Permission),
+					SourceType:      string(ps.Source.Typename),
+					TeamName:        string(ps.Source.Team.Name),
+					TeamID:          string(ps.Source.Team.ID),
+					RepositoryOwner: string(ps.Source.Repository.Owner.Login),
+					RepositoryName:  string(ps.Source.Repository.Name),
+				}
+				sources = append(sources, permissionSource)
+			}
+			collaborator := Collaborator{
+				Login:               string(coll.Node.Login),
+				EffectivePermission: string(coll.Permission),
+				Sources:             sources,
+			}
+			repository.Collaborators = append(repository.Collaborators, collaborator)
+		}
+		return query.Repository.Collaborators.PageInfo
+	})
+}
+
+type BranchProtectionRule struct {
+	Pattern                      string   `json:"pattern,omitempty"`
+	MatchingRefs                 []string `json:"matching_refs,omitempty"`
+	AllowsForcePushes            bool     `json:"allows_force_pushes,omitempty"`
+	AllowsDeletions              bool     `json:"allows_deletions,omitempty"`
+	RequiredApprovingReviewCount int      `json:"required_approving_review_count,omitempty"`
+	RequiredStatusCheckContexts  []string `json:"required_status_check_contexts,omitempty"`
+	RequiresApprovingReviews     bool     `json:"requires_approving_reviews,omitempty"`
+	RequiresCodeOwnerReviews     bool     `json:"requires_code_owner_reviews,omitempty"`
+	RequiresCommitSignatures     bool     `json:"requires_commit_signatures,omitempty"`
+	RequiresLinearHistory        bool     `json:"requires_linear_history,omitempty"`
+	RequiresStrictStatusChecks   bool     `json:"requires_strict_status_checks,omitempty"`
+	IsAdminEnforced              bool     `json:"is_admin_enforced,omitempty"`
+	RestrictsReviewDismissals    bool     `json:"restricts_review_dismissals,omitempty"`
+	DismissesStaleReviews        bool     `json:"dismisses_stale_reviews,omitempty"`
+}
+
+func (s *GithubClient) LoadRepositoryBranchProtectionRules(repositories ...*Repository) error {
+	for _, repository := range repositories {
+		if err := s.loadRepositoryBranchProtectionRules(repository); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadRepositoryBranchProtectionRules(repository *Repository) error {
+	var query struct {
+		Repository struct {
+			BranchProtectionRules struct {
+				PageInfo PageInfo
+				Nodes    []struct {
+					Pattern      githubv4.String
+					MatchingRefs struct {
+						Nodes []struct {
+							Name githubv4.String
+						}
+					} `graphql:"matchingRefs(first: 100)"`
+					AllowsForcePushes            githubv4.Boolean
+					AllowsDeletions              githubv4.Boolean
+					RequiredApprovingReviewCount githubv4.Int
+					RequiredStatusCheckContexts  []githubv4.String
+					RequiresApprovingReviews     githubv4.Boolean
+					RequiresCodeOwnerReviews     githubv4.Boolean
+					RequiresCommitSignatures     githubv4.Boolean
+					RequiresLinearHistory        githubv4.Boolean
+					RequiresStrictStatusChecks   githubv4.Boolean
+					IsAdminEnforced              githubv4.Boolean
+					RestrictsReviewDismissals    githubv4.Boolean
+					DismissesStaleReviews        githubv4.Boolean
+				}
+			} `graphql:"branchProtectionRules(first: 100, after: $cursor)"`
+		} `graphql:"repository(owner: $owner, name: $repo)"`
+	}
+	return s.Query(&query).Str("owner", repository.Owner).Str("repo", repository.Name).Cursor("cursor").Run(func() PageInfo {
+		log.L().Info().Bool("next-page", bool(query.Repository.BranchProtectionRules.PageInfo.HasNextPage)).Str("repo", repository.Name).Msg("Fetched next page")
+		for _, r := range query.Repository.BranchProtectionRules.Nodes {
+			var matchingRefs []string
+			for _, ref := range r.MatchingRefs.Nodes {
+				matchingRefs = append(matchingRefs, string(ref.Name))
+			}
+			var statusChecks []string
+			for _, ref := range r.RequiredStatusCheckContexts {
+				statusChecks = append(statusChecks, string(ref))
+			}
+			rule := BranchProtectionRule{
+				Pattern:                      string(r.Pattern),
+				MatchingRefs:                 matchingRefs,
+				AllowsForcePushes:            bool(r.AllowsForcePushes),
+				AllowsDeletions:              bool(r.AllowsDeletions),
+				RequiredApprovingReviewCount: int(r.RequiredApprovingReviewCount),
+				RequiredStatusCheckContexts:  statusChecks,
+				RequiresApprovingReviews:     bool(r.RequiresApprovingReviews),
+				RequiresCodeOwnerReviews:     bool(r.RequiresCodeOwnerReviews),
+				RequiresCommitSignatures:     bool(r.RequiresCommitSignatures),
+				RequiresLinearHistory:        bool(r.RequiresLinearHistory),
+				RequiresStrictStatusChecks:   bool(r.RequiresStrictStatusChecks),
+				IsAdminEnforced:              bool(r.IsAdminEnforced),
+				RestrictsReviewDismissals:    bool(r.RestrictsReviewDismissals),
+				DismissesStaleReviews:        bool(r.DismissesStaleReviews),
+			}
+			repository.BranchProtectionRules = append(repository.BranchProtectionRules, rule)
+		}
+		return query.Repository.BranchProtectionRules.PageInfo
+	})
+}
+
+func (s *GithubClient) LoadRepositorySecurityConfig(repositories ...*Repository) error {
+	for _, repository := range repositories {
+		if err := s.loadSecurityConfig(repository); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadSecurityConfig(repository *Repository) error {
+	if enabled, _, err := s.v3Client.Repositories.GetVulnerabilityAlerts(context.Background(), repository.Owner, repository.Name); err == nil {
+		repository.VulnerabilityAlerts = enabled
+	} else {
+		return err
+	}
+	return nil
+}
+
+func (s *GithubClient) EnableVulnerabilityAlerts(owner string, repository string) error {
+	_, err := s.v3Client.Repositories.EnableVulnerabilityAlerts(context.Background(), owner, repository)
+	return err
+}
+
+type Workflow struct {
+	Name         string `json:"name,omitempty"`
+	Path         string `json:"path,omitempty"`
+	State        string `json:"state,omitempty"`
+	UsageWindows int64  `json:"usage_windows,omitempty"`
+	UsageUbuntu  int64  `json:"usage_ubuntu,omitempty"`
+	UsageMac     int64  `json:"usage_mac,omitempty"`
+}
+
+func (s *GithubClient) LoadRepositoryWorkflows(repositories ...*Repository) error {
+	for _, repository := range repositories {
+		if err := s.loadRepositoryWorkflows(repository); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *GithubClient) loadRepositoryWorkflows(repository *Repository) error {
+	return s.paginateGithub3(func(lo gh3.ListOptions) (*gh3.Response, error) {
+		workflows, resp, err := s.v3Client.Actions.ListWorkflows(context.Background(), repository.Owner, repository.Name, &lo)
+		for _, w := range workflows.Workflows {
+			usage, _, err := s.v3Client.Actions.GetWorkflowUsageByID(context.Background(), repository.Owner, repository.Name, w.GetID())
+			if err != nil {
+				return nil, err
+			}
+			repository.Workflows = append(repository.Workflows, Workflow{
+				Name:         w.GetName(),
+				Path:         w.GetPath(),
+				State:        w.GetState(),
+				UsageMac:     usage.GetBillable().GetMacOS().GetTotalMS(),
+				UsageUbuntu:  usage.GetBillable().GetUbuntu().GetTotalMS(),
+				UsageWindows: usage.GetBillable().GetWindows().GetTotalMS(),
+			})
 		}
 		return resp, err
 	})
-	return result, err
 }
 
-func (s *GithubClient) CreateRepository(org string, r Repository) error {
+func (s *GithubClient) CreateRepository(org string, r *Repository) error {
 	_, _, err := s.v3Client.Repositories.Create(context.Background(), org, &gh3.Repository{
 		Name:        &r.Name,
 		Private:     &r.Private,
